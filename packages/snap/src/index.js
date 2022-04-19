e690a1c2bece540f1b2f1dff16402068146c46eb
Frederik Bolding <frederik.bolding@gmail.com>
2022-04-19 16:05:02 +0200

    Initial commit

---
+++ packages/snap/src/index.js
@@ -0,0 +1,19 @@
+wallet.registerRpcMessageHandler(async (originString, requestObject) => {
+  switch (requestObject.method) {
+    case 'hello':
+      return wallet.request({
+        method: 'snap_confirm',
+        params: [
+          {
+            prompt: `Hello, ${originString}!`,
+            description:
+              'This custom confirmation is just for display purposes.',
+            textAreaContent:
+              'But you can edit the snap source code to make it do something, if you want to!',
+          },
+        ],
+      });
+    default:
+      throw new Error('Method not found.');
+  }
+});
