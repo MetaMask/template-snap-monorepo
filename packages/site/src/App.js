1d8c2b192489fd12f7d4cccbbe21b3765ae943de
Frederik Bolding <frederik.bolding@gmail.com>
2022-04-19 16:36:01 +0200

    More basic setup

--- packages/site/src/App.js
+++ packages/site/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState();
+
+  const snapId = `local:http://localhost:8080`;
+
+  const connect = async () => {
+    await window.ethereum.request({
+      method: 'wallet_enable',
+      params: [{
+        wallet_snap: { [snapId]: {} },
+      }]
+    })
+  }
+
+  const sendHello = async () => {
+    try {
+      const response = await window.ethereum.request({
+        method: 'wallet_invokeSnap',
+        params: [snapId, {
+          method: 'hello'
+        }]
+      })
+      setState(response);
+    } catch (err) {
+      console.error(err)
+      alert('Problem happened: ' + err.message || err)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, Snaps!</h1>
+      <details>
+        <summary>Instructions</summary>
+        <ul>
+          <li>First, click "Connect". Then, try out the other buttons!</li>
+          <li>Please note that:</li>
+          <ul>
+            <li>
+              The <code>snap.manifest.json</code> and <code>package.json</code> must be located in the server root directory..
+            </li>
+            <li>
+              The Snap bundle must be hosted at the location specified by the <code>location</code> field of <code>snap.manifest.json</code>.
+            </li>
+          </ul>
+        </ul>
+      </details>
+      <br />
+
+      <button onClick={connect}>Connect</button>
+      <button onClick={sendHello}>Send Hello</button>
+
+      <br />
+
+      {state && `Snap replied with: ${state}`}
     </div>
   );
 }
