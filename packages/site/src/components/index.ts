6d4233b89c55983b35393db0691ae1435000847b
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-10-13 19:51:58 +0200

    Use Gatsby instead of react-scripts

--- packages/site/src/components/index.ts
+++ packages/site/src/components/index.ts
@@ -1,4 +1,8 @@
-export { Header } from './Header';
-export { Footer } from './Footer';
-export { Toggle } from './Toggle';
-export { Home } from './Home';
+export * from './Buttons';
+export * from './Card';
+export * from './Footer';
+export * from './Header';
+export * from './MetaMask';
+export * from './PoweredBy';
+export * from './SnapLogo';
+export * from './Toggle';
