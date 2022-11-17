865e768ae816c92fa59892af97e1b21d9deaed57
Frederik Bolding <frederik.bolding@gmail.com>
2022-11-17 11:39:42 +0100

    Fix wallet_invokeSnap

--- packages/site/src/utils/snap.ts
+++ packages/site/src/utils/snap.ts
@@ -58,12 +58,7 @@ export const getSnap = async (version?: string): Promise<Snap | undefined> => {
 export const sendHello = async () => {
   await window.ethereum.request({
     method: 'wallet_invokeSnap',
-    params: [
-      defaultSnapOrigin,
-      {
-        method: 'hello',
-      },
-    ],
+    params: { snapId: defaultSnapOrigin, request: { method: 'hello', } }
   });
 };
 
