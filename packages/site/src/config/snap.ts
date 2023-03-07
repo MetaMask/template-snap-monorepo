9dfeaf8131cbb1cceb412e9a7dbd14013c602dad
Guillaume Roux <guillaumeroux123@gmail.com>
2023-03-07 14:31:10 +0100

    Update README to reference Gatsby (#33)

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -3,4 +3,4 @@
  * Will default to the local hosted snap if no value is provided in environment.
  */
 export const defaultSnapOrigin =
-  process.env.REACT_APP_SNAP_ORIGIN ?? `local:http://localhost:8080`;
+  process.env.SNAP_ORIGIN ?? `local:http://localhost:8080`;
