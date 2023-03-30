04854cd8604c1758fe1579a6e68e0fd9c107f2d7
Ziad Saab <ziad.saab@gmail.com>
2023-03-30 09:06:11 -0400

    different fix for Buffer

--- packages/snap/snap.config.js
+++ packages/snap/snap.config.js
@@ -3,4 +3,23 @@ module.exports = {
     src: './src/index.ts',
     port: 8080,
   },
+  bundlerCustomizer: (bundler) => {
+    // eslint-disable-next-line node/global-require
+    const through = require('through2');
+    bundler.transform(function () {
+      let data = '';
+      return through(
+        function (buf, _enc, cb) {
+          data += buf;
+          cb();
+        },
+        // eslint-disable-next-line consistent-return
+        function (cb) {
+          this.push("globalThis.Buffer = require('buffer/').Buffer;");
+          this.push(data);
+          cb();
+        },
+      );
+    });
+  },
 };
--- packages/snap/src/buffer-fix.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
-globalThis.Buffer = require('buffer/').Buffer;
-
-export {};
