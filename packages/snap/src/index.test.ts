1d2bf16eadaa2004f51a3f0acab8340a165d1231
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-06-28 10:38:14 +0200

    Add test for invalid method

--- packages/snap/src/index.test.ts
+++ packages/snap/src/index.test.ts
@@ -28,4 +28,25 @@ describe('onRpcRequest', () => {
 
     expect(await response).toRespondWith(true);
   });
+
+  it('throws an error if the requested method does not exist', async () => {
+    const { request, close } = await installSnap();
+
+    const response = await request({
+      method: 'foo',
+    });
+
+    expect(response).toRespondWithError({
+      code: -32603,
+      message: 'Internal JSON-RPC error.',
+      data: {
+        cause: {
+          message: 'Method not found.',
+          stack: expect.any(String),
+        },
+      },
+    });
+
+    await close();
+  });
 });
