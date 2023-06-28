9c9b5fffc63dbca73c4e006ac2a653a95f42c77e
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-06-28 12:34:51 +0200

    Update test name

--- packages/snap/src/index.test.ts
+++ packages/snap/src/index.test.ts
@@ -3,30 +3,32 @@ import { expect } from '@jest/globals';
 import { panel, text } from '@metamask/snaps-ui';
 
 describe('onRpcRequest', () => {
-  it('hello', async () => {
-    const { request } = await installSnap();
+  describe('hello', () => {
+    it('shows a confirmation dialog', async () => {
+      const { request } = await installSnap();
 
-    const origin = 'Jest';
-    const response = request({
-      method: 'hello',
-      origin,
+      const origin = 'Jest';
+      const response = request({
+        method: 'hello',
+        origin,
+      });
+
+      const ui = await response.getInterface();
+      expect(ui.type).toBe('confirmation');
+      expect(ui).toRender(
+        panel([
+          text(`Hello, **${origin}**!`),
+          text('This custom confirmation is just for display purposes.'),
+          text(
+            'But you can edit the snap source code to make it do something, if you want to!',
+          ),
+        ]),
+      );
+
+      await ui.ok();
+
+      expect(await response).toRespondWith(true);
     });
-
-    const ui = await response.getInterface();
-    expect(ui.type).toBe('confirmation');
-    expect(ui).toRender(
-      panel([
-        text(`Hello, **${origin}**!`),
-        text('This custom confirmation is just for display purposes.'),
-        text(
-          'But you can edit the snap source code to make it do something, if you want to!',
-        ),
-      ]),
-    );
-
-    await ui.ok();
-
-    expect(await response).toRespondWith(true);
   });
 
   it('throws an error if the requested method does not exist', async () => {
