d9e31ad752f2eace1c66d81dc22bd3b430a6c07a
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-10-31 12:21:21 +0100

    Remove publishing workflows

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,8 +4,5 @@ set -u
 set -o pipefail
 
 rm .github/CODEOWNERS
-rm .github/workflows/create-release-pr.yml
-rm .github/workflows/publish-release.yml
-rm .github/workflows/publish-site.yml
 rm -f scripts/cleanup.sh
 git commit -am "Clean up undesired MetaMask GitHub files"
