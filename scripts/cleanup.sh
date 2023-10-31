7b0bca8159f17357edba2f2417babb80908a80a4
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-10-31 11:37:32 +0100

    Remove publish-site.yml in cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,5 +6,6 @@ set -o pipefail
 rm .github/CODEOWNERS
 rm .github/workflows/create-release-pr.yml
 rm .github/workflows/publish-release.yml
+rm .github/workflows/publish-site.yml
 rm -f scripts/cleanup.sh
 git commit -am "Clean up undesired MetaMask GitHub files"
