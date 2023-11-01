6c0cc43fe900e32049ac0dd48cfac343d1195282
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-11-01 12:02:20 +0100

    Fix lint configs

--- packages/snap/.eslintrc.js
+++ packages/snap/.eslintrc.js
@@ -1,5 +1,18 @@
 module.exports = {
   extends: ['../../.eslintrc.js'],
 
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+  },
+
+  overrides: [
+    {
+      files: ['snap.config.ts'],
+      extends: [
+        '@metamask/eslint-config-nodejs',
+      ],
+    }
+  ],
+
   ignorePatterns: ['!.eslintrc.js', 'dist/'],
 };
